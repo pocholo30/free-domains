cd5f01040979621c99fc3c6b91e31df5ff443546
pocholo30 <145801208+pocholo30@users.noreply.github.com>
2023-12-07 18:46:13 +0100

    Update pocholo.is-an.app.js

--- domains/pocholo.is-an.app.js
+++ domains/pocholo.is-an.app.js
@@ -1,14 +1,13 @@
 addSubDomain({
-  description: 'A domain for my Home Server Dashboard', // describe your project in this field
-  domain: 'in-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'pocholo', // desired subdomain name
+  description: 'A domain for my Home Server Dashboard',
+  domain: 'in-an.app',
+  subdomain: 'pocholo',
   owner: {
     repo: 'https://github.com/pocholo30',
     email: 'poxhi30@gmail.com',
   },
   record: {
-    //CNAME: '<cname-domain>', // e.g.: <your-github-account>.github.io
     NS: ['ns1.cloudns.net', 'ns2.cloudns.net', 'ns3.cloudns.net', 'ns4.cloudns.net'],
   },
-  //proxy: false,
+  proxy: false,
 })
